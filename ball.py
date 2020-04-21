fe78d856a98a2a092f0484f4d75a3b512cc618fe
JakubBekier <44197747+JakubBekier@users.noreply.github.com>
2020-04-21 13:22:52 +0200

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,8 +1,9 @@
 class Ball:
-    def __init__(self, x, y, x_speed, y_speed, radius):
+    def __init__(self, x, y, x_speed, y_speed, radius, freeze=False):
         self.x = x
         self.y = y
         self.x_speed = x_speed
         self.y_speed = y_speed
         self.radius = radius
+        self.freeze = freeze
 
