118e304f1305da7db3c636ea0bbb2048b7a9777f
JakubBekier <44197747+JakubBekier@users.noreply.github.com>
2020-04-21 13:24:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
-from PIL import Image
+from PIL import Image                             # Można wywalić, wtedy w,h ustaw na 788 i 444
 
-from classes import *
+from player import *
 from ball import Ball
 
 pygame.init()
@@ -12,15 +12,19 @@ image = Image.open("tlo3.png")
 w, h = image.size
 win = pygame.display.set_mode((w, h))
 
-player1 = Player(50, 414, False, 10, 0, 0, 30)
-player2 = Player(w - 50, 414, False, 10, 0, 0, 30)
-ball = Player(100, 200, False, 0, 0, 0, 20)
+player1 = Player(50, 414, False, 10, 30, 0)
+player2 = Player(w - 50, 414, False, 10, 30, 0)
+ball = Ball(100, 200, 0, 0, 20)
+
+player1_points = 0
+player2_points = 0
+
 run = True
 while run:
-    pygame.time.delay(30)
+    pygame.time.delay(2)
+    ball = redraw_game_window(win, bg, player1, player2, ball)
     player1, player2, ball = movement(player1, player2, ball, w)
     redraw_game_window(win, bg, player1, player2, ball)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
