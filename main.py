5f91a5bd17fb9f83a1b5f8c4750770dae3070460
JakubBekier <44197747+JakubBekier@users.noreply.github.com>
2020-04-24 13:46:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from PIL import Image                             # Można wywalić, wtedy w,h ustaw na 788 i 444
+from PIL import Image
 
 from player import *
 from ball import Ball
@@ -14,19 +14,24 @@ win = pygame.display.set_mode((w, h))
 
 player1 = Player(50, 414, False, 10, 30, 0)
 player2 = Player(w - 50, 414, False, 10, 30, 0)
+playertest = Player(w/2, 414, False, 10, 30, 0)
 ball = Ball(100, 200, 0, 0, 20)
+balltest = Ball(w/2, 200, 0, 0, 20)
 
 player1_points = 0
 player2_points = 0
 
+print(w, h)
 run = True
 while run:
-    pygame.time.delay(2)
+    pygame.time.delay(30)
     ball = redraw_game_window(win, bg, player1, player2, ball)
     player1, player2, ball = movement(player1, player2, ball, w)
-    redraw_game_window(win, bg, player1, player2, ball)
+    # balltest = redraw_game_window_test(win, bg, playertest, balltest)
+    # playertest, ball = movement_test(playertest, balltest, w)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+    print(playertest.jumpcount, balltest.y_speed)
+    # print(playertest.x, playertest.y, balltest.x, balltest.y, balltest.y_speed);
 pygame.quit()
