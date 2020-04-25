0a3fcfced4ed4e9cbc7572a1a60da8d2c5f89f4d
JakubBekier <44197747+JakubBekier@users.noreply.github.com>
2020-04-25 20:49:59 +0200

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,3 +1,5 @@
+import pygame
+
 class Ball:
     def __init__(self, x, y, x_speed, y_speed, radius, freeze=False):
         self.x = x
@@ -6,4 +8,7 @@ class Ball:
         self.y_speed = y_speed
         self.radius = radius
         self.freeze = freeze
+        self.firstTouch = False
 
+    def draw(self,win):
+        pygame.draw.circle(win, (255, 255, 255), (int(self.x), int(self.y)), self.radius)
